95feeb4ccdd546f224107dcbbc723c93e075b28f
Pablo Olmos <82683589+PualMark@users.noreply.github.com>
2022-02-22 00:16:20 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,26 +8,13 @@
     }
 
 //Smooth Scroll
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 300, function(){
-
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+$(".navbar ul a").on("click", function (e) {
+  // 1
+  e.preventDefault();
+  // 2
+  const href = $(this).attr("href");
+  // 3
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 300);
 });
 
 //Email
